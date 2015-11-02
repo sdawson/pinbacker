3ea345217aa913e33cabac2260f5bf5f0c40bb53
Sophie Dawson <sophiecdawson@gmail.com>
2015-11-01 22:05:25 -0800

    Use auth token instead of password

--- pinbacker.sh
+++ pinbacker.sh
@@ -3,11 +3,11 @@
 
 backup_dir="$HOME/bkup/pinboard"
 username="username goes here"
-password="password goes here"
+auth_token="auth token goes here"
 
 echo -n "Backing up Pinboard bookmarks...."
 
-curl -s -g "https://$username:$password@api.pinboard.in/v1/posts/all?format=json" \
+curl -s -g "https://api.pinboard.in/v1/posts/all?format=json&auth_token=$username:$auth_token" \
   -o "$backup_dir/pinboard-bookmarks-`date "+%Y%m%d%H%M"`.json"
 
 echo "Done."
